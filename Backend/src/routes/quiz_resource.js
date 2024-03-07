1eea4a060134399287a8fbc63c03b5913ca2b09c
roger-niyomugabo <rogerniyomugabo3@gmail.com>
2024-03-07 02:38:10 +0200

    created quiz

--- Backend/src/routes/quiz_resource.js
+++ Backend/src/routes/quiz_resource.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { Quiz } from '../db/models/Quiz.model';
+import output from '../utils/response';
 const router = express.Router();
 
 // function to create a quiz
 router.post('/', async (req, res) => {
-
+  const quiz = await Quiz.create(req.body);
+  return output(res, 201, 'Quiz created successfully', quiz, null);
 });
 
 // function to get all quizes
